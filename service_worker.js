a0a9c200391a7e9abadbeea6f2490ac2d5085db7
SimonLab <simon.labondance@gmail.com>
2019-12-16 15:58:02 +0000

    update sw cache link

--- service_worker.js
+++ service_worker.js
@@ -1,20 +1,20 @@
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-      caches.open('airhorner').then(function(cache) {
-        return cache.addAll([
-          '/manifest.json',
-          '/elm.js',
-          '/dwyl.png'
-        ]);
-      })
-    );
-   });
-   
-   self.addEventListener('fetch', function(event) {
-       console.log(event.request.url);
-       event.respondWith(
-           caches.match(event.request).then(function(response) {
-             return response || fetch(event.request);
-           })
-         );
-   });
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open('airhorner').then(function (cache) {
+      return cache.addAll([
+        '/elm-pwa-example/manifest.json',
+        '/elm-pwa-example/elm.js',
+        '/elm-pwa-example/dwyl.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
